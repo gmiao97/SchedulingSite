f7aba08dc9e4f97706004228896fddb7603b1720
George Miao <gmiao97@gmail.com>
2020-03-31 20:19:41 -0400

    added fields to user serializer to support update of student/teacher profile

--- yoyaku/serializers.py
+++ yoyaku/serializers.py
@@ -7,13 +7,13 @@ from .models import StudentProfile, TeacherProfile, Event, Subject, MyUser
 class StudentProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = StudentProfile
-        fields = ['school_name', 'school_grade']
+        fields = ['id', 'school_name', 'school_grade']
 
 
 class TeacherProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = TeacherProfile
-        fields = ['association']
+        fields = ['id', 'association']
 
 
 class TimezoneField(serializers.Field):
@@ -27,12 +27,14 @@ class TimezoneField(serializers.Field):
 class MyUserSerializer(serializers.ModelSerializer):
     student_profile = StudentProfileSerializer(many=False, required=False, allow_null=True)
     teacher_profile = TeacherProfileSerializer(many=False, required=False, allow_null=True)
+    student_id = serializers.PrimaryKeyRelatedField(write_only=True, allow_null=True, required=False, queryset=StudentProfile.objects.all())
+    teacher_id = serializers.PrimaryKeyRelatedField(write_only=True, allow_null=True, required=False, queryset=TeacherProfile.objects.all())
     time_zone = TimezoneField()
 
     class Meta:
         model = MyUser
         fields = ['id', 'first_name', 'last_name', 'email', 'password', 'user_type', 'time_zone', 'phone_number',
-                  'birthday', 'description', 'student_profile', 'teacher_profile']
+                  'birthday', 'description', 'student_profile', 'teacher_profile', 'student_id', 'teacher_id']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -51,14 +53,28 @@ class MyUserSerializer(serializers.ModelSerializer):
         return user
 
     def update(self, instance, validated_data):  # TODO update student/teacher profile
-        student_profile = validated_data.pop('student_profile')
-        teacher_profile = validated_data.pop('teacher_profile')
+        student_data = validated_data.pop('student_profile')
+        teacher_data = validated_data.pop('teacher_profile')
+        student_instance = validated_data.pop('student_id')
+        teacher_instance = validated_data.pop('teacher_id')
 
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.email = validated_data.get('email', instance.email)
         instance.time_zone = validated_data.get('time_zone', instance.time_zone)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.birthday = validated_data.get('birthday', instance.birthday)
+        instance.description = validated_data.get('description', instance.description)
         instance.save()
+
+        if student_instance:
+            student_instance.school_name = student_data.get('school_name', student_instance.school_name)
+            student_instance.school_grade = student_data.get('school_grade', student_instance.school_grade)
+            student_instance.save()
+        if teacher_instance:
+            teacher_instance.association = teacher_data.get('association', teacher_instance.association)
+            teacher_instance.save()
+
         return instance
 
 
