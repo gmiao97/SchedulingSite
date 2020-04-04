82512b8266b0d549e839da7e1bd0b9ac03b27631
George Miao <gmiao97@gmail.com>
2020-04-04 11:40:34 -0400

    emable HTTPS prod

--- yoyakusite/prod_settings.py
+++ yoyakusite/prod_settings.py
@@ -6,8 +6,7 @@ from .settings import *
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
-# TODO enable SSL
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 django_heroku.settings(locals())
