ae6c0430ad1399177d89f34981ccda823acfcfae
George Miao <gmiao97@gmail.com>
2020-05-25 19:56:37 -0400

    file upload and download for production

--- yoyakusite/prod_settings.py
+++ yoyakusite/prod_settings.py
@@ -30,9 +30,9 @@ STATICFILES_FINDERS = (
 )
 
 # SECRET_KEY = os.environ['SECRET_KEY']
-# DEBUG = False
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# django_heroku.settings(locals())
+DEBUG = False
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+django_heroku.settings(locals())
