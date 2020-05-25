71530331297b82107d9b3d85062e74531b50dfef
George Miao <gmiao97@gmail.com>
2020-05-25 19:47:53 -0400

    file upload and download for production

--- yoyaku/models.py
+++ yoyaku/models.py
@@ -125,5 +125,5 @@ class Event(models.Model):
     recurrence = models.ForeignKey(Recurrence, null=True, on_delete=models.CASCADE, related_name='recurrenceEvents',
                                    related_query_name='recurrenceEvent')
     comment = models.CharField(_('comment'), max_length=1000, null=True, blank=True)
-    file = models.FileField(upload_to='eventFiles/', null=True)
+    file = models.FileField(upload_to='hsw14yh841sr/public/media/eventFiles/', null=True)
 
--- yoyakusite/prod_settings.py
+++ yoyakusite/prod_settings.py
@@ -15,22 +15,24 @@ AWS_QUERYSTRING_AUTH = False
 
 AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
 
+AWS_DEFAULT_ACL = None
+
 #static media settings
-STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/hsw14yh841sr/public/'
-MEDIA_URL = STATIC_URL + 'media/'
+MEDIA_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+# MEDIA_URL = STATIC_URL
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # STATIC_ROOT = 'staticfiles'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-SECRET_KEY = os.environ['SECRET_KEY']
-DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-django_heroku.settings(locals())
+# SECRET_KEY = os.environ['SECRET_KEY']
+# DEBUG = False
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# django_heroku.settings(locals())
