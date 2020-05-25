788ef001269a1618b389182f11bc18e0c81d5a3f
George Miao <gmiao97@gmail.com>
2020-05-24 21:38:35 -0400

    file upload and download for production

--- yoyakusite/prod_settings.py
+++ yoyakusite/prod_settings.py
@@ -3,29 +3,29 @@ import os
 
 from .settings import *
 
-INSTALLED_APPS += ['storages']
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-AWS_ACCESS_KEY_ID = os.environ.get("CLOUDCUBE_ACCESS_KEY_ID", "")
-AWS_SECRET_ACCESS_KEY = os.environ.get("CLOUDCUBE_SECRET_ACCESS_KEY", "")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("CLOUDCUBE_STORAGE_BUCKET_NAME", "")
-
-AWS_QUERYSTRING_AUTH = False
-
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
-
-#static media settings
-STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/hsw14yh841sr/public/'
-MEDIA_URL = STATIC_URL + 'media/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = 'staticfiles'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+# INSTALLED_APPS += ['storages']
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+#
+# AWS_ACCESS_KEY_ID = os.environ.get("CLOUDCUBE_ACCESS_KEY_ID", "")
+# AWS_SECRET_ACCESS_KEY = os.environ.get("CLOUDCUBE_SECRET_ACCESS_KEY", "")
+# AWS_STORAGE_BUCKET_NAME = os.environ.get("CLOUDCUBE_STORAGE_BUCKET_NAME", "")
+#
+# AWS_QUERYSTRING_AUTH = False
+#
+# AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
+#
+# #static media settings
+# STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/hsw14yh841sr/public/'
+# MEDIA_URL = STATIC_URL + 'media/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATIC_ROOT = 'staticfiles'
+# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+#
+# STATICFILES_FINDERS = (
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# )
 
 SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
