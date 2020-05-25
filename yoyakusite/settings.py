349fd4aab30185a845ff40dd623a0e1563737aae
George Miao <gmiao97@gmail.com>
2020-05-24 22:11:17 -0400

    file upload and download for production

--- yoyakusite/settings.py
+++ yoyakusite/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
