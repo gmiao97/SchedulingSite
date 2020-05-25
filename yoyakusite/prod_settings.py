c301fad5af35e4b45a085ce637f4b493af2e65a2
George Miao <gmiao97@gmail.com>
2020-05-25 19:52:08 -0400

    file upload and download for production

--- yoyakusite/prod_settings.py
+++ yoyakusite/prod_settings.py
@@ -5,7 +5,7 @@ from .settings import *
 
 INSTALLED_APPS += ['storages']
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_ACCESS_KEY_ID = os.environ.get("CLOUDCUBE_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.environ.get("CLOUDCUBE_SECRET_ACCESS_KEY", "")
